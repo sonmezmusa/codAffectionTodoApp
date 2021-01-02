a7bf713877d9dd8046c99b67deb3851a388db185
sonmezmusa <sonmezmusa@outlook.com>
2021-01-02 22:16:30 +0300

    todos/urls.py değiştirildi
    list_todo_items, insert_todo_item viewler için path eklendi.

--- todos/urls.py
+++ todos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('list/', list_todo_items, name='list')
+    path('list/', list_todo_items, name='list'),
+    path('insert_todo/', insert_todo_item, name='insert_todo_item'),
 ]
