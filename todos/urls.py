397cc8d0d915ee38ee01ee56f8a65feb78a3ea6c
sonmezmusa <sonmezmusa@outlook.com>
2021-01-02 22:53:53 +0300

    todos/urls.py değiştirildi
    todo delete için path eklendi.

--- todos/urls.py
+++ todos/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('list/', list_todo_items, name='list'),
     path('insert_todo/', insert_todo_item, name='insert_todo_item'),
+    path('delete_todo/<int:todo_id>', delete_todo_item, name='delete_todo_item'),
 ]
