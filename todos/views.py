6ab7fd7305c2348a3f1f6d93111b559b59cefbfd
sonmezmusa <sonmezmusa@outlook.com>
2021-01-02 21:39:10 +0300

    appStructure.txt, todoProject/settings.py, todoProject/urls.py, todos/views.py değiştirildi

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def list_todo_items(request):
+    return render(request, 'todos/todo_list.html')
