e096a321e2c6e5421b67868468ca8b7900cf1590
sonmezmusa <sonmezmusa@outlook.com>
2021-01-06 23:50:22 +0300

    todoProject/settings.py değiştirildi
    geçici postgresql alternatifi olarak sqlite3 eklendi.

--- todoProject/settings.py
+++ todoProject/settings.py
@@ -75,14 +75,25 @@ WSGI_APPLICATION = 'todoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TodoDB',
-        'USERNAME': 'postgres',
-        'PASSWORD': 'test123',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'TodoDB',
+#         'USERNAME': 'postgres',
+#         'PASSWORD': 'test123',
+#     }
+# }
 # 'HOST': 'localhost',
 # >postgres@musaUbuntu:~$ createuser --interactive 
 # Enter name of role to add: musa
